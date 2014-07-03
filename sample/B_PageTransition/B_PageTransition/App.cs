276f24d76e258fe0ede04eedf1eca03f889aa264
Hironobu OKUYAMA <okuokuoku@gmail.com>
2014-07-03 16:36:11 +0900

    Add PageTransition sample

---
+++ sample/B_PageTransition/B_PageTransition/App.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace B_PageTransition
+{
+    public class App
+    {
+        public static Page GetMainPage()
+        {	
+            var btn = new Button
+            {
+                Text = "Goto Second",
+                Font = Font.SystemFontOfSize(30),
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            var navPage = new NavigationPage(new ContentPage 
+            {
+                Title = "The First",
+                Content = btn
+            });
+
+            btn.Clicked += (sender, e) => 
+                navPage.PushAsync(App.GetSecondPage());
+                
+            return navPage;
+        }
+
+        static Page GetSecondPage()
+        {
+            return new ContentPage
+            {
+                Title = "The Second",
+                BackgroundColor = Color.Aqua,
+                Content = new Label
+                {
+                    Text = "This is second",
+                    Font = Font.SystemFontOfSize(30),
+                    HorizontalOptions = LayoutOptions.Center,
+                    VerticalOptions = LayoutOptions.Center
+                }
+            };
+        }
+    }
+}
+
