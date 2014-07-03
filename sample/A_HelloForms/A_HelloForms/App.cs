250cd075ea4e2c00d2fb3b76bb8e33dd6dfabb3e
Hironobu OKUYAMA <okuokuoku@gmail.com>
2014-07-03 16:12:56 +0900

    Add HelloForms sample

---
+++ sample/A_HelloForms/A_HelloForms/App.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace A_HelloForms
+{
+    public class App
+    {
+        public static Page GetMainPage()
+        {	
+            return new ContentPage
+            { 
+                Content = new DatePicker
+                {
+                    VerticalOptions = LayoutOptions.CenterAndExpand,
+                    HorizontalOptions = LayoutOptions.CenterAndExpand,
+                },
+            };
+        }
+    }
+}
+
