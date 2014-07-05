324deb97a50e7d5db8ddbcc1ec8797d001bdd1a3
amay077 <okuokuoku@gmail.com>
2014-07-05 15:34:17 +0900

    Delete  unused code

--- sample/D_FormsInNativeView/iOS/AppDelegate.cs
+++ sample/D_FormsInNativeView/iOS/AppDelegate.cs
@@ -46,23 +46,5 @@ namespace D_FormsInNativeView.iOS
         {
         }
     }
-
-//    [Register("AppDelegate")]
-//    public partial class AppDelegate : UIApplicationDelegate
-//    {
-//        UIWindow window;
-//
-//        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
-//        {
-//            Forms.Init();
-//
-//            window = new UIWindow(UIScreen.MainScreen.Bounds);
-//			
-//            window.RootViewController = App.GetMainPage().CreateViewController();
-//            window.MakeKeyAndVisible();
-//			
-//            return true;
-//        }
-//    }
 }
 
